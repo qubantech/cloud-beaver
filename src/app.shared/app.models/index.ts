a282e9fd01f509007c58a46e8ca4825b4c1eb099
Lihogub Oleg <oligli@yandex.ru>
2022-04-22 19:01:19 +0300

    added user model

--- src/app.shared/app.models/index.ts
+++ src/app.shared/app.models/index.ts
@@ -1,1 +1,12 @@
-export {}
+type User = {
+    id: string,
+    sex: 'm' | 'f',
+    nickname: string,
+    firstname: string,
+    lastname: string,
+    email: string,
+    role: 'manager' | 'partner' | 'implementer',
+    projects: string[], // projects id's
+}
+
+export type { User }

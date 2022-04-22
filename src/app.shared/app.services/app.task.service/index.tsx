77de76eba3b8c529aff12f1bfbf570c50f8959c2
Lihogub Oleg <oligli@yandex.ru>
2022-04-22 21:53:57 +0300

    added AddConference, AddTask methods

--- src/app.shared/app.services/app.task.service/index.tsx
+++ src/app.shared/app.services/app.task.service/index.tsx
@@ -1,9 +1,16 @@
 import {Task} from '../../app.models'
 import {useWatchedObject} from '../../app.configs/hooks'
-
+import {putObject} from '../../app.configs'
+// @ts-ignore
+import {v4 as uuid} from 'uuid'
 
 const useTaskById = (id: string) => useWatchedObject<Task>(`/tasks/${id}`)
 
 const useTaskList = () => useWatchedObject<{[key: string]: Task}>('/tasks/')
 
-export {useTaskById, useTaskList}
+const addTask = (task: Task) => {
+	task.id = uuid()
+	return putObject<Task>(`/tasks/${task.id}`, task)
+}
+
+export {useTaskById, useTaskList, addTask}

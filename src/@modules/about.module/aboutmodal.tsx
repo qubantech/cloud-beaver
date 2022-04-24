254067ad424f1261442e9c2c61ee69759cca3ae4
Nikita Vanyuchenko <postamentbox@bk.ru>
2022-04-24 11:15:56 +0300

    fix cards

--- src/@modules/about.module/aboutmodal.tsx
+++ src/@modules/about.module/aboutmodal.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
-import {Modal, Image, Text, Grid, Group, createStyles, Textarea, Button, TextInput, SimpleGrid} from '@mantine/core'
+import {
+	Modal,
+	Image,
+	Text,
+	Grid,
+	Group,
+	createStyles,
+	Textarea,
+	Button,
+	TextInput,
+	SimpleGrid,
+	Timeline, Card, Paper, Spoiler, Badge
+} from '@mantine/core'
+import {GitBranch} from 'tabler-icons-react'
 
 const useStyles = createStyles((theme) => {
 	const BREAKPOINT = theme.fn.smallerThan('sm')
@@ -58,7 +71,7 @@ const useStyles = createStyles((theme) => {
 		},
 
 		title: {
-			marginBottom: theme.spacing.xl * 1.5,
+			marginBottom: theme.spacing.xl,
 			fontFamily: `Greycliff CF, ${theme.fontFamily}`,
 
 			[BREAKPOINT]: {
@@ -74,6 +87,49 @@ const useStyles = createStyles((theme) => {
 	}
 })
 
+const СheckCard = ({
+				  title,
+				  date,
+				  status,
+				  onClick = () => {return}
+			  }: {
+	title: string,
+	date?: string,
+	status: 'completed' | 'late' | 'cancelled' | 'current' | 'planned'
+	onClick?: () => void
+}) => {
+
+	const statusColor =
+		status == 'completed' ? 'green' :
+			status == 'late' ? 'red' :
+				status == 'cancelled' ? 'red' :
+					status == 'current' ? 'yellow' :
+						status == 'planned' ? 'violet' : ''
+
+	const dateTitle =
+		status == 'completed' ? `Завершено ${date}` :
+			status == 'late' ? 'Задерживается' :
+				status == 'cancelled' ? 'Отменено' :
+					status == 'current' ? `В работе с ${date}` :
+						status == 'planned' ? `Запланировано на ${date}` : ''
+
+
+	return <Paper p={'xs'} style={{ cursor: 'pointer' }}>
+		<SimpleGrid cols={1}>
+			<Text color="dimmed" size="sm" mb={0} style={{ color: '#bbbbbb' }}>
+				{title}
+			</Text>
+			<Group position={'apart'}>
+				<Badge color={statusColor}>
+					<Text size="xs" mt={4}>
+						{dateTitle}
+					</Text>
+				</Badge>
+			</Group>
+		</SimpleGrid>
+	</Paper>
+}
+
 const Aboutmodal = (props:{isOpen:boolean, setOpen: (b:boolean) => void, title:string, imageurl:string, description:string}) => {
 	const { classes } = useStyles()
 	return(
@@ -86,14 +142,46 @@ const Aboutmodal = (props:{isOpen:boolean, setOpen: (b:boolean) => void, title:s
 			<Image height={400} src={props.imageurl}/>
 			<Grid>
 				<Grid.Col span={6}>
-					<Text pt={25}>
-						{props.description}
+					<Text size="lg" weight={700} pt={25} className={classes.title}>
+						План работы
 					</Text>
+					<Timeline pt={1} active={1} bulletSize={24} lineWidth={2}>
+						<Timeline.Item bullet={<GitBranch size={12}/>} title="Контрольная точка 1">
+							<СheckCard date={'10.02.2022'}
+								  title={'Создание системы администрирования городского парковочного пространства'}
+								  status={'completed'}
+							/>
+						</Timeline.Item>
+						<Timeline.Item bullet={<GitBranch size={12}/>} title="Контрольная точка 2">
+							<СheckCard date={'22.03.2022'}
+								  title={'Интеллектуальное управление городским общественным транспортом'}
+								  status={'completed'}
+							/>
+						</Timeline.Item>
+						<Timeline.Item bullet={<GitBranch size={12}/>} title="Контрольная точка 3">
+							<СheckCard date={'2022'}
+								  title={'Создание безопасных и комфортных мест ожидания общественного транспорта'}
+								  status={'planned'}
+							/>
+						</Timeline.Item>
+						<Timeline.Item bullet={<GitBranch size={12}/>} title="Контрольная точка 4">
+							<СheckCard date={'2022'}
+								  title={'Создание системы мониторинга состояния дорожного полотна'}
+								  status={'planned'}
+							/>
+						</Timeline.Item>
+					</Timeline>
 				</Grid.Col>
 				<Grid.Col span={6}>
 					<form className={classes.form} onSubmit={(event) => event.preventDefault()}>
 						<Text size="lg" weight={700} className={classes.title}>
-							Оставить предложение
+							Описание
+						</Text>
+						<Text>
+							{props.description}
+						</Text>
+						<Text pt={25} pb={25} size="lg" weight={700} className={classes.title}>
+							Оставить предложение и узнать подробнее
 						</Text>
 						<div className={classes.fields}>
 							<SimpleGrid cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>

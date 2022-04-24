86d8f45010ef20b2943ebfaee3e05bad9f287af5
uliatitova <tulia_87@mail.ru>
2022-04-24 11:20:10 +0300

    analytics tab added

--- src/@modules/projects.module/components/recommendation.component/recommendation.component.tsx
+++ src/@modules/projects.module/components/recommendation.component/recommendation.component.tsx
@@ -1,29 +1,42 @@
-import React from 'react'
-import { Button, Paper, Text, Group, CloseButton } from '@mantine/core'
+import React, {useState} from 'react'
+import {Button, Paper, Text, Group, CloseButton} from '@mantine/core'
 
 export function Recommendation() {
+	const [recStart, setRecStart] = useState(false)
+	const [isOpened, setIsOpened] = useState(true)
+
 	return (
-		<Paper withBorder p="lg" radius="md" shadow="md">
-			<Group position="apart" mb="xs">
-				<Text size="md" weight={500}>
-					Allow cookies
-				</Text>
-				<CloseButton mr={-9} mt={-9} />
-			</Group>
-			<Text color="dimmed" size="xs">
-				So the deal is, we want to spy on you. We would like to know what did you have for todays
-				breakfast, where do you live, how much do you earn and like 50 other things. To view our
-				landing page you will have to accept all cookies. That&apos;s all, and remember that we are
-				watching...
-			</Text>
-			<Group position="right" mt="xs">
-				<Button variant="default" size="xs">
-					Cookies preferences
-				</Button>
-				<Button variant="outline" size="xs">
-					Accept all
-				</Button>
-			</Group>
-		</Paper>
+		<>
+			{
+				isOpened &&
+				<Paper withBorder p="lg" radius="md" shadow="md" style={{minWidth: '300px'}}>
+					<Group position="apart" mb="xs">
+						<Text size="md" weight={500}>
+							Рекомендации
+						</Text>
+						<CloseButton mr={-9} mt={-9} onClick={() => setIsOpened(false)}/>
+					</Group>
+					{
+						!recStart &&
+						<Group position="left" mt="xs">
+							<Button variant="default" size="xs">
+								Отказаться
+							</Button>
+							<Button variant="outline" size="xs" onClick={() => setRecStart(true)}>
+								Запустить
+							</Button>
+						</Group>
+						||
+						<Group>
+							<Text>
+								Рекомендуется перераспределить ресурсы с проектов Туризм и сервис и Инновации для городской среды
+								на проекты Умное ЖКХ и Городское управление
+							</Text>
+						</Group>
+					}
+				</Paper>
+			}
+		</>
+
 	)
 }
